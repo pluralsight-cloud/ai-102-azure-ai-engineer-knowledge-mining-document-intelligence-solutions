89193291af4765d98ff63cc4275350ee2ac13da2
mattuLA <matthew-ulasien@pluralsight.com>
2024-08-13 12:54:46 -0500

    updated doc intel demo files

--- demos/document_intelligence/rest_api-examples/analyze_document.sh
+++ demos/document_intelligence/rest_api-examples/analyze_document.sh
@@ -1,4 +1,4 @@
 curl -v -i POST "{AI_SERVICE_ENDPOINT}/documentintelligence/documentModels/{modelId}:analyze?api-version=2024-02-29-preview" 
 -H "Content-Type: application/json" 
 -H "Ocp-Apim-Subscription-Key: {AI_SERVICE_KEY}" 
---data-ascii "{'urlSource': '{your-document-url}'}"
+--data-ascii "{'urlSource': 'https://raw.githubusercontent.com/pluralsight-cloud/ai-102-azure-ai-engineer-knowledge-mining-document-intelligence-solutions/main/demos/document_intelligence/receipt_image/invoice_sample.jpg'}"
