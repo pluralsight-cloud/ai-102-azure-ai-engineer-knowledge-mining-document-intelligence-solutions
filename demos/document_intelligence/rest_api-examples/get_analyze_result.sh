17463f8dafea1963fb8b2cb1e69a9272a3d32b0e
mattuLA <matthew-ulasien@pluralsight.com>
2024-08-13 13:27:40 -0500

    updated docintel demo

--- demos/document_intelligence/rest_api-examples/get_analyze_result.sh
+++ demos/document_intelligence/rest_api-examples/get_analyze_result.sh
@@ -1,2 +1,2 @@
-curl -v -X GET "{AI_SERVICE_ENDPOINT}/documentintelligence/documentModels/{modelId}/analyzeResults/{resultId}?api-version=2024-02-29-preview" 
--H "Ocp-Apim-Subscription-Key: {AI_SERVICE_KEY}"
+curl -v -X GET "${AI_SERVICE_ENDPOINT}/documentintelligence/documentModels/{model-id}/analyzeResults/{resultId}?api-version=2024-02-29-preview" \
+-H "Ocp-Apim-Subscription-Key: ${AI_SERVICE_KEY}"
